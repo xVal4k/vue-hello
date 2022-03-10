a7844baca71132a2d6049f67a8221dd2c7343929
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-10 13:31:12 +0100

    fix word to world :)

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const app = new Vue ({
     el: "#app",
     data: {
-        saluto: "Hello word!",
+        saluto: "Hello world!",
         src: "https://picsum.photos/200/300",
         alt: "First image with vue"
     }
